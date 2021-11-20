72ecaee2f9e8f2bc6d37e0bdc2c8b044947fedbf
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2021-11-20 21:22:03 +0100

    Added swap3 function and removed redundant sorting .

--- fuzzy.h
+++ fuzzy.h
@@ -10,25 +10,17 @@ using real_t = double;
 
 class TriFuzzyNum {
 	public:
+        constexpr real_t lower_value() const { return l; }
+        constexpr real_t modal_value() const { return m; }
+        constexpr real_t upper_value() const { return u; }
+
         constexpr TriFuzzyNum(real_t a, real_t b, real_t c) {
-            if (a > b) {std::swap (a, b);}
-            if (b > c) {std::swap (b, c);}
-            if (a > b) {std::swap (a, b);}
+            swap3(a, b, c);
             l = a;
             m = b;
             u = c;
         }
 
-        constexpr real_t lower_value() const { return l; }
-        constexpr real_t modal_value() const { return m; }
-        constexpr real_t upper_value() const { return u; }
-        constexpr real_t z () const { return (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l)); }
-        constexpr real_t x () const { return ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z(); }
-        constexpr real_t y () const { return (u - l) / z();}
-        constexpr real_t first () const { return x() - y() / 2; }
-        constexpr real_t second () const { return 1 - y(); }
-        constexpr real_t third () const { return m; }
-                
         constexpr std::partial_ordering operator<=>(const TriFuzzyNum &other) const {
             if (auto cmp = this->first() <=> other.first(); cmp != 0)
                 return cmp;
@@ -39,36 +31,19 @@ class TriFuzzyNum {
         }
         
         constexpr bool operator== (const TriFuzzyNum& other) const = default;
+        constexpr bool operator!= (const TriFuzzyNum& other) const = default;
         		
         constexpr TriFuzzyNum& operator+=(const TriFuzzyNum& other) {
-            real_t a = this->l + other.l;
-            real_t b = this->m + other.m;
-            real_t c = this->u + other.u;
-
-            if (a > b) {std::swap(a, b); }
-            if (b > c) {std::swap(b, c); }
-            if (a > b) {std::swap(a, b); }
-
-            this->l = a;
-            this->m = b;
-            this->u = c;
-
+            this->l = this->l - other.l;
+            this->m = this->m - other.m;
+            this->u = this->u - other.u;
             return *this;
         }
 
-        constexpr TriFuzzyNum& operator-=(const TriFuzzyNum& other) {
-            real_t a = this->l - other.u;
-            real_t b = this->m - other.m;
-            real_t c = this->u - other.l;
-
-            if (a > b) {std::swap(a, b); }
-            if (b > c) {std::swap(b, c); }
-            if (a > b) {std::swap(a, b); }
-
-            this->l = a;
-            this->m = b;
-            this->u = c;
-
+        constexpr TriFuzzyNum& operator-=(const TriFuzzyNum &other) {
+            this->l = this->l - other.u;
+            this->m = this->m - other.m;
+            this->u = this->u - other.l;
             return *this;
         }
 
@@ -76,15 +51,10 @@ class TriFuzzyNum {
             real_t a = this->l * other.l;
             real_t b = this->m * other.m;
             real_t c = this->u * other.u;
-
-            if (a > b) {std::swap(a, b); }
-            if (b > c) {std::swap(b, c); }
-            if (a > b) {std::swap(a, b); }
-
+            swap3(a, b, c);
             this->l = a;
             this->m = b;
             this->u = c;
-
             return *this;
         }
 
@@ -108,6 +78,19 @@ class TriFuzzyNum {
 		real_t l;
 		real_t m;
 		real_t u;
+
+        constexpr real_t z () const { return (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l)); }
+        constexpr real_t x () const { return ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z(); }
+        constexpr real_t y () const { return (u - l) / z();}
+        constexpr real_t first () const { return x() - y() / 2; }
+        constexpr real_t second () const { return 1 - y(); }
+        constexpr real_t third () const { return m; }
+
+        constexpr void  swap3 (real_t& a, real_t& b, real_t& c) {
+            if (a > b) { std::swap(a, b); }
+            if (b > c) { std::swap(b, c); }
+            if (a > b) { std::swap(a, b); }
+        }
 };
 
 consteval TriFuzzyNum crisp_number(real_t v) {
