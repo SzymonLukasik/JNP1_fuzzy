3ad3d4a2c27048eb5f5bdc800dff3e907937dee2
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2021-11-20 20:32:21 +0100

    AAddedMichał's changes to TriFuzzyNumm

--- fuzzy.h
+++ fuzzy.h
@@ -17,74 +17,89 @@ class TriFuzzyNum {
             l = a;
             m = b;
             u = c;
-
-            // We want to get these via functions with same signatures as lower_value
-            real_t z = (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l));
-            real_t x = ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z;
-            real_t y = (u - l) / z;
-            first = x - y / 2;
-            second = 1 - y;
-            third = m;
         }
 
-        constexpr real_t lower_value() const {
-            return l;
-        }
-
-        constexpr real_t modal_value() const {
-            return m;
-        }
-
-        constexpr real_t upper_value() const {
-            return u;
-        }
+        constexpr real_t lower_value() const { return l; }
+        constexpr real_t modal_value() const { return m; }
+        constexpr real_t upper_value() const { return u; }
+        constexpr real_t z () const { return (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l)); }
+        constexpr real_t x () const { return ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z(); }
+        constexpr real_t y () const { return (u - l) / z();}
+        constexpr real_t first () const { return x() - y() / 2; }
+        constexpr real_t second () const { return 1 - y(); }
+        constexpr real_t third () const { return m; }
                 
-        constexpr std::partial_ordering operator<=>(const TriFuzzyNum& other) const {
-            if (auto cmp = this->first <=> other.first; cmp != 0)
+        constexpr std::partial_ordering operator<=>(const TriFuzzyNum &other) const {
+            if (auto cmp = this->first() <=> other.first(); cmp != 0)
                 return cmp;
-            if (auto cmp = this->second <=> other.second; cmp != 0)
+            if (auto cmp = this->second() <=> other.second(); cmp != 0)
                 return cmp;
-            auto cmp = this->third <=> other.third;
+            auto cmp = this->third() <=> other.third();
             return cmp;
         }
         
         constexpr bool operator== (const TriFuzzyNum& other) const = default;
         		
-        constexpr TriFuzzyNum& operator+= (const TriFuzzyNum& other) {
-            this->l += other.l;
-            this->m += other.m;
-            this->u += other.u;
+        constexpr TriFuzzyNum& operator+=(const TriFuzzyNum& other) {
+            real_t a = this->l + other.l;
+            real_t b = this->m + other.m;
+            real_t c = this->u + other.u;
+
+            if (a > b) {std::swap(a, b); }
+            if (b > c) {std::swap(b, c); }
+            if (a > b) {std::swap(a, b); }
+
+            this->l = a;
+            this->m = b;
+            this->u = c;
+
             return *this;
         }
 
-        constexpr TriFuzzyNum& operator-= (const TriFuzzyNum& other) {
-            this->l -= other.u;
-            this->m -= other.m;
-            this->u -= other.l;
+        constexpr TriFuzzyNum& operator-=(const TriFuzzyNum& other) {
+            real_t a = this->l - other.l;
+            real_t b = this->m - other.m;
+            real_t c = this->u - other.u;
+
+            if (a > b) {std::swap(a, b); }
+            if (b > c) {std::swap(b, c); }
+            if (a > b) {std::swap(a, b); }
+
+            this->l = a;
+            this->m = b;
+            this->u = c;
+
             return *this;
         }
 
-        constexpr TriFuzzyNum& operator*= (const TriFuzzyNum& other) {
-            this->l *= other.l;
-            this->m *= other.m;
-            this->u *= other.u;
+        constexpr TriFuzzyNum& operator*=(const TriFuzzyNum& other) {
+            real_t a = this->l * other.l;
+            real_t b = this->m * other.m;
+            real_t c = this->u * other.u;
+
+            if (a > b) {std::swap(a, b); }
+            if (b > c) {std::swap(b, c); }
+            if (a > b) {std::swap(a, b); }
+
+            this->l = a;
+            this->m = b;
+            this->u = c;
+
             return *this;
         }
 
-        constexpr const TriFuzzyNum operator+ (const TriFuzzyNum& other) const {
-            return TriFuzzyNum(*this) += other;
+        
+
+        constexpr const TriFuzzyNum operator+(const TriFuzzyNum& other) const {
+            return (TriFuzzyNum(*this) += other);
         }
 
-        constexpr const TriFuzzyNum operator- (const TriFuzzyNum& other) const {
-            TriFuzzyNum result = *this;
-            result -= other;
-            return result;
+        constexpr const TriFuzzyNum operator-(const TriFuzzyNum& other) const {
+            return (TriFuzzyNum(*this) -= other);
         }
 
-        constexpr const TriFuzzyNum operator* (const TriFuzzyNum& other) const {
-            TriFuzzyNum result = *this;
-            result *= other;
-            return result;
+        constexpr const TriFuzzyNum operator*(const TriFuzzyNum& other) const {
+            return (TriFuzzyNum(*this) *= other);
         }
 
         friend std::ostream& operator<< (std::ostream& os, const TriFuzzyNum& num);
@@ -93,9 +108,6 @@ class TriFuzzyNum {
 		real_t l;
 		real_t m;
 		real_t u;
-		real_t first;
-		real_t second;
-		real_t third;
 };
 
 consteval TriFuzzyNum crisp_number(real_t v) {
